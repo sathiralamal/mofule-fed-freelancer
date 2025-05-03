ea9524f1e0c337a2da996f9a7116158c16800beb
Sathira Lamal <it18045840@sliit.lk>
2025-05-03 13:05:52 +0530

    refactor headerbar component: enhance layout and styling, implement user profile integration, and update search functionality

--- apps/shell/src/app/components/headerbar/headerbar.component.ts
+++ apps/shell/src/app/components/headerbar/headerbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,6 +8,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-headerbar',
@@ -24,10 +27,29 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     FormsModule,
     MatButtonModule,
     MatIconModule,
+    MatChipsModule,
+    MatMenuModule,
   ],
   templateUrl: './headerbar.component.html',
   styleUrl: './headerbar.component.css',
 })
-export class HeaderbarComponent {
-  value = '';
+export class HeaderbarComponent implements OnInit {
+  authService = inject(AuthService);
+  userName: string = '';
+  userEmail: string = '';
+  userPicture: string = '';
+  profileLoaded: boolean = false;
+
+  ngOnInit() {
+    console.log('UserProfileComponent initialized');
+    this.authService.user$.subscribe((user) => {
+      console.log('User Profile:', user);
+      if (user) {
+        this.userName = user.name || '';
+        this.userEmail = user.email || '';
+        this.userPicture = user.picture || '';
+        this.profileLoaded = true;
+      }
+    });
+  }
 }
